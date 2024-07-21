fe06d209f64d744bce1a11a0cbbfeec132664c71
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-21 15:21:15 +0500

    error parsing go mod

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module gitlab.com/idoko/bucketeer
 
 go 1.22.5
+
+require github.com/go-chi/render v1.0.3
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
-
 import (
-	"fmt"
+    "context"
+    "fmt"
+    "gitlab.com/idoko/bucketeer/db"
+    "gitlab.com/idoko/bucketeer/handler"
+    "log"
+    "net"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 )
-
 func main() {
-	fmt.Println("Hello World!")
+    addr := ":8080"
+    listener, err := net.Listen("tcp", addr)
+    if err != nil {
+        log.Fatalf("Error occurred: %s", err.Error())
+    }
+    dbUser, dbPassword, dbName :=
+        os.Getenv("POSTGRES_USER"),
+        os.Getenv("POSTGRES_PASSWORD"),
+        os.Getenv("POSTGRES_DB")
+    database, err := db.Initialize(dbUser, dbPassword, dbName)
+    if err != nil {
+        log.Fatalf("Could not set up database: %v", err)
+    }
+    defer database.Conn.Close()
+
+    httpHandler := handler.NewHandler(database)
+    server := &http.Server{
+        Handler: httpHandler,
+    }
+    go func() {
+        server.Serve(listener)
+    }()
+    defer Stop(server)
+    log.Printf("Started server on %s", addr)
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+    log.Println(fmt.Sprint(<-ch))
+    log.Println("Stopping API server.")
+}
+func Stop(server *http.Server) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    if err := server.Shutdown(ctx); err != nil {
+        log.Printf("Could not shut down server correctly: %v\n", err)
+        os.Exit(1)
+    }
 }
